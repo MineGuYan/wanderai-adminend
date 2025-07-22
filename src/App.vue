2c3ce5b2605a6a89347b582a81e9809ce14db080
ZhaoWenZhuo <167313298+MineGuYan@users.noreply.github.com>
2025-07-22 08:36:29 +0800

    新增JWT校验

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
+function checkJWT() {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return
+  }
+
+  const JWTsplit = token.split('.');
+
+  if(JWTsplit.length !== 3) {
+    localStorage.removeItem('token');
+    return;
+  }
+
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (payload.exp < currentTime) {
+      localStorage.removeItem('token');
+    }
+  } catch (error) {
+    console.error('Invalid JWT token:', error);
+    localStorage.removeItem('token');
+  }
+
+}
+
+onMounted(() => {
+  checkJWT()
+});
 </script>
 
 <template>
